9c9e40f0593a39421afe66c372263791abbb6bbe
julien <julien.aerts2@gmail.com>
2021-11-27 19:34:40 +0100

    Amélioration du wordcloud avec un module en js Manque un traitement sur le traitement des valeurs envoyées

--- templates/static/scripts/wordcloud.js
+++ templates/static/scripts/wordcloud.js
@@ -1,37 +1,25 @@
-import * as d3 from "https://cdn.skypack.dev/d3@7";
+function wordcloud(text) {
+    console.log("text : " . text )
+    anychart.onDocumentReady(function () {
+        // code to create a word cloud chart will be here
+        var data = unescape(text)
+        console.log(data)
 
-var d3 = require("d3"),
-    cloud = require("../");
 
-var layout = cloud()
-    .size([500, 500])
-    .words([
-      "Hello", "world", "normally", "you", "want", "more", "words",
-      "than", "this"].map(function(d) {
-      return {text: d, size: 10 + Math.random() * 90, test: "haha"};
-    }))
-    .padding(5)
-    .rotate(function() { return ~~(Math.random() * 2) * 90; })
-    .font("Impact")
-    .fontSize(function(d) { return d.size; })
-    .on("end", draw);
+        // create a tag (word) cloud chart
+        var chart = anychart.tagCloud(data);
 
-layout.start();
+        // set a chart title
+        chart.title('15 most spoken languages')
+        // set an array of angles at which the words will be laid out
+        chart.angles([0])
+        // enable a color range
+        chart.colorRange(true);
+        // set the color range length
+        chart.colorRange().length('80%');
 
-function draw(words) {
-  d3.select("body").append("svg")
-      .attr("width", layout.size()[0])
-      .attr("height", layout.size()[1])
-    .append("g")
-      .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
-    .selectAll("text")
-      .data(words)
-    .enter().append("text")
-      .style("font-size", function(d) { return d.size + "px"; })
-      .style("font-family", "Impact")
-      .attr("text-anchor", "middle")
-      .attr("transform", function(d) {
-        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-      })
-      .text(function(d) { return d.text; });
+        // display the word cloud chart
+        chart.container("container");
+        chart.draw();
+    });
 }
